a1d483dfb5dcf15f6bae83de6c25da09b55c87ce
Bodya <bodya.9787@gmail.com>
2017-02-22 14:44:52 +0200

    add methods to recipe service #89

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 import { Recipe } from '../recipe';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   providers: [ShoppingListService]
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
-  private recipeIndex: number = 1;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  selectedRecipe: Recipe;
+  private recipeIndex: number;
+  private subscription: Subscription;
 
-  constructor(private sls: ShoppingListService, private router: Router) { }
+  constructor(private sls: ShoppingListService,
+              private router: Router, 
+              private route: ActivatedRoute,
+              private recipesService: RecipeService) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
+      }
+    );
   }
 
   onEdit() {
@@ -23,11 +35,16 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onDelete(){
+    this.recipesService.deleteRecipe(this.selectedRecipe);
     this.router.navigate(['/recipes']);
   }
 
   onAddToShoppingList() {
   	this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
